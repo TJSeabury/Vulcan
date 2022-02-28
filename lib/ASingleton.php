f8148c747cb42838e5cf4a7d1670c093bca041c0
TJSeabury <tylerseabury@gmail.com>
2022-02-28 11:27:45 -0500

    added singleton

---
+++ lib/ASingleton.php
@@ -0,0 +1,18 @@
+<?php namespace VUlcan\lib;
+
+/**
+ * @see: https://stackoverflow.com/questions/1818765/extend-abstract-singleton-class
+ */
+abstract class ASingleton {
+    private static $_instances = array();
+
+    public static function getInstance() {
+        $class = get_called_class();
+        if ( !isset( self::$_instances[$class] ) ) {
+            self::$_instances[$class] = new $class();
+        }
+        return self::$_instances[$class];
+    }
+
+    abstract protected function __construct();
+}
