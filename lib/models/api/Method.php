24642471535c5f4753169a95879c58e4e8a66995
TJSeabury <tylerseabury@gmail.com>
2022-03-01 17:09:16 -0500

    created method model

---
+++ lib/models/api/Method.php
@@ -0,0 +1,53 @@
+<?php namespace Vulcan\lib\models\api;
+
+class Method {
+
+    protected static $validVerbs = [
+        'GET',
+        'POST',
+        'PATCH',
+        'PUT',
+        'DELETE',
+    ];
+
+    protected static function verbsAreValid( array $verbs ): bool {
+        foreach ( $verbs as $verb ) {
+            if ( ! in_array(
+                strtoupper( $verb ),
+                static::$validVerbs
+            ) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public string $verbs;
+
+    public function formatVerbs(): string {
+        return implode(
+            ', ',
+            array_map(
+                fn($v) => strtoupper($v),
+                $this->verbs
+            )
+        );
+    }
+
+    protected $action;
+
+    public function __construct( array $verbs, callable $action ) {
+        if ( ! static::verbsAreValid( $verbs ) ) {
+            throw new \Error( 'Invalid HTTP verbs! ' . $verbs );
+        }
+        $this->verbs = $verbs;
+        $this->action = $action;
+    }
+
+    public function action( \WP_REST_Request $request ): void {
+        if ( ! is_callable( $this->action ) ) {
+            throw new \Error( 'Action is not callable!' );
+        }
+        $this->action( $request );
+    }
+}
