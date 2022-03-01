f49412bdb07f582f0cbf0414982fa6eba553d27f
TJSeabury <tylerseabury@gmail.com>
2022-03-01 17:09:07 -0500

    created config for defaults

---
+++ lib/models/api/Config.php
@@ -0,0 +1,16 @@
+<?php namespace Vulcan\lib\models\api;
+
+class Config {
+    public static $defaults = [
+        'namespace' => 'vulcan',
+        'version' => 'v1',
+        'requireAuth' => true
+    ];
+    public static function hasSetting( string $key ) {
+        if ( !empty( $key ) && 
+        true === array_key_exists( $key, self::$defaults ) ) {
+            return true;
+        }
+        return false;
+    }
+}
